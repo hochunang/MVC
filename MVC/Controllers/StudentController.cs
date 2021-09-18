e70143296898040f15a2b0c6319f02cd65dbc1ec
hochunang <hochunang@gmail.com>
2021-09-18 15:58:41 +0800

    Add project files.

---
+++ MVC/Controllers/StudentController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    [Authorize]
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            using (DatabaseEntities db = new DatabaseEntities())
+            {
+                return View(db.Tables.ToArray());
+            }
+            
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            using (DatabaseEntities db = new DatabaseEntities())
+            {
+                return View(db.Tables.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(Table student)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                using (DatabaseEntities db = new DatabaseEntities())
+                {
+                    db.Tables.Add(student);
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (DatabaseEntities db = new DatabaseEntities())
+            {
+                return View(db.Tables.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Table student)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using (DatabaseEntities db = new DatabaseEntities())
+                {
+                    db.Entry(student).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (DatabaseEntities db = new DatabaseEntities())
+            {
+                return View(db.Tables.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using (DatabaseEntities db = new DatabaseEntities())
+                {
+                    Table student = db.Tables.Where(x => x.Id == id).FirstOrDefault();
+                    db.Tables.Remove(student);
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user, string ReturnUrl)
+        {
+            DatabaseEntities db = new DatabaseEntities();
+            User newUser = db.Users.Where(x => x.Username == user.Username && x.Password == user.Password)
+                .FirstOrDefault();
+            if (newUser != null)
+            {
+                FormsAuthentication.SetAuthCookie(user.Username, false);
+                return Redirect(ReturnUrl);
+            }
+            else
+            {
+                return View(user);
+            }
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                using (DatabaseEntities db = new DatabaseEntities())
+                {
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                }
+                Response.Write("<script LANGUAGE='JavaScript' >alert('Register Successfully')</script>");
+                return RedirectToAction("Login");
+            }
+            catch
+            {
+                Response.Write("<script LANGUAGE='JavaScript' >alert('Invalid Register')</script>");
+                return View();
+            }
+
+           
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("/User/Login");
+          
+        }
+    }
+}
